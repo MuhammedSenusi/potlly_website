01cc2a4339d788207e545c906aeca630778b8b99
Yusuf Iysah  | UX Engineer <yusufiysah@gmail.com>
2026-09-12 16:28:31 +0100

    Revert "feat: switch to local font loading for Epilogue and Plus Jakarta Sans; update build script to use webpack"
    This reverts commit 8a39b37105ca688c38b15afcd72b82306ce88622.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,22 @@
 import type { Metadata, Viewport } from "next";
-import localFont from "next/font/local";
+import { Epilogue, Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import { SiteHeader } from "@/components/site/site-header";
 import { SiteFooter } from "@/components/site/site-footer";
 import { Reveal } from "@/components/site/reveal";
 import { site } from "@/constants/site";
 
-const epilogue = localFont({
+const epilogue = Epilogue({
   variable: "--font-epilogue",
-  src: "./_og/epilogue-bold.ttf",
-  weight: "700",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800"],
   display: "swap",
 });
 
-const jakarta = localFont({
+const jakarta = Plus_Jakarta_Sans({
   variable: "--font-jakarta",
-  src: [
-    { path: "../public/fonts/PlusJakartaSans-Regular.ttf", weight: "400" },
-    { path: "../public/fonts/PlusJakartaSans-Medium.ttf", weight: "500" },
-    { path: "../public/fonts/PlusJakartaSans-SemiBold.ttf", weight: "600" },
-    { path: "../public/fonts/PlusJakartaSans-Bold.ttf", weight: "700" },
-  ],
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
   display: "swap",
 });
 
